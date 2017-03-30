afe123a9af5c7f91c9222d878985941dade027f6
codonculous <feidameng01@gmail.com>
2017-03-30 12:44:41 -0400

    Create class method: total funds

--- bankaccount.rb
+++ bankaccount.rb
@@ -1,46 +1,47 @@
 class Bankaccount
 
+#class
   @@accounts = []
+  @@total_funds = 0
 
   def self.create
     @@accounts.push(Bankaccount.new)
   end
 
+  def self.total_funds
+
+
+    @@accounts.each do |account|
+
+        @@total_funds += @@account.@balance
+
+    end
+
+  end
+
+  def self.interest_time
+
+  end
+
+#instance
+
   def initialize
 
-    @balance = 0
+    @balance = 0.00
 
   end
 
   def deposit(m_in)
-    @balance += m_in
+    @balance += m_in.to_f
   end
 
   def withdraw(m_out)
-    @balance -= m_out
+    @balance -= m_out.to_f
   end
 
 
 
 end
 
-#
-# Add a class variable called @@interest_rate that is a float representing the interest rate for all the accounts in the bank.
 
-# This will eventually store the list of all bank accounts in the bank.
-# Add an initialize instance method that sets @balance to zero.
-# You should also add an attr_accessor for balance to your class.
-# Balance is stored in an instance variable because the value needs to be different from account to account.
-# Add an instance method called deposit that accepts a number as an argument and adds that amount to the account's balance.
-# This needs to be an instance method because it pertains to a single, specific account.
-# Add an instance method called withdraw that accepts a number as an argument and subtracts that amount from the account's balance.
-
-# Add a class method called create that calls BankAccount.new
-# and adds the new object to @@accounts so that we can find it again in the future.
-# This method should return the new account object.
-
-# This needs to be a class method because at the time we run it there is no single,
-# specific account object that we are working on.
-# Add a class method called total_funds that returns the sum of all balances across all accounts in @@accounts.
-# This needs to be a class method because it does not pertain to any single, specific account.
 # Add a class method called interest_time that iterates through all accounts and increases their balances according to @@interest_rate.
