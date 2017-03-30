f005a08cb6da7a7b5a02c9d0f144a52275e1b826
codonculous <feidameng01@gmail.com>
2017-03-30 18:49:28 -0400

    book class finished

---
+++ book.rb
@@ -0,0 +1,105 @@
+class Book
+
+  #class
+  @@on_shelf = []
+  @@on_loan  = []
+  @@overdue_books = []
+
+  def self.create(title,author,isbn)
+    book = Book.new(title, author, isbn)
+    @@on_shelf.push(book)
+  end
+
+  def self.current_due_date
+    Time.now + 60*60*24*7
+  end
+
+  def self.overdue_books
+    overdue_books = @@on_loan.select {|book| book.due_date > Time.now}
+    overdue_books.each {|book| @@overdue_books.push(book.title)}
+  end
+
+  def self.browse
+    @@on_shelf.shuffle[1]
+  end
+
+  def self.available
+
+    @@on_shelf
+  end
+
+  def self.borrowed
+
+    return @@on_loan
+
+  end
+
+  # def self.display_available
+  #   @@on_shelf.map {|book| book.title}
+  # end
+
+  def self.update_shelf(shelf)
+    @@on_shelf = shelf
+  end
+
+  def self.update_loan(loan)
+    @@on_loan = loan
+  end
+
+  #instance
+
+  attr_accessor :due_date
+  attr_reader :title, :author, :isbn
+
+  def initialize(title, author, isbn)
+
+    @title  = title
+    @author = author
+    @isbn   = isbn
+    # @date_borrowed = Time.new
+    # @due_date = Time.new
+    #@book_keep_dates = 60*60*24*7
+
+  end
+
+  def borrow
+    if lent_out?
+      return puts "Borrowed"
+    else
+
+      bb = Book.available.delete( self )
+      Book.update_shelf(Book.available)
+      Book.update_loan( Book.borrowed.push(bb) )
+
+    end
+  end
+
+
+  def return_to_library
+    if lent_out?
+      rb = Book.available.push( self )
+      Book.update_shelf(rb)
+      Book.update_loan.delete(rb)
+      @due_date = ""
+    else
+      puts "Can't return book not being lent"
+    end
+
+  end
+
+  def due_date
+    @due_date = Book.current_due_date
+  end
+
+  def lent_out?
+
+    if Book.borrowed.include?( self )
+    # if Book.borrowed.include?( Book.borrowed.select {|book| book.isbn == @isbn} )
+      return true
+    else
+      return false
+    end
+
+  end
+
+end
--- test.rb
+++ test.rb
@@ -1,21 +1,46 @@
-require_relative "bankaccount"
+# require_relative "bankaccount"
+#
+# Bankaccount.create("feida1")
+# Bankaccount.create("feida2")
+# Bankaccount.create("feida3")
+#
+# Bankaccount.all_accounts[0].deposit(1000)
+# Bankaccount.all_accounts[1].deposit(2000)
+# Bankaccount.all_accounts[2].deposit(3000)
+#
+# Bankaccount.total_funds
+#
+# Bankaccount.interest_time(0.0)
+#
+# Bankaccount.total_funds
+#
+# Bankaccount.all_accounts[0].withdraw(1000/2)
+# Bankaccount.all_accounts[1].withdraw(2000/2)
+# Bankaccount.all_accounts[2].withdraw(3000/2)
+#
+# Bankaccount.total_funds
 
-Bankaccount.create("feida1")
-Bankaccount.create("feida2")
-Bankaccount.create("feida3")
 
-Bankaccount.all_accounts[0].deposit(1000)
-Bankaccount.all_accounts[1].deposit(2000)
-Bankaccount.all_accounts[2].deposit(3000)
+require_relative "book"
+Book.create("starwarsI","Lucas","ISBN434523452")
+Book.create("starwarsII","Lucas","ISBN434523453")
+Book.create("starwarsIII","Lucas","ISBN434523454")
 
-Bankaccount.total_funds
+Book.display_available
+book = Book.find_available("blah")
+book.borrow
 
-Bankaccount.interest_time(0.0)
+puts "Book available: #{Book.available}"
+star_wars = Book.available
+star_wars.borrow
+Book.available[0].borrow
+Book.available[0].borrow
+puts "Book available: #{Book.available}"
 
-Bankaccount.total_funds
-
-Bankaccount.all_accounts[0].withdraw(1000/2)
-Bankaccount.all_accounts[1].withdraw(2000/2)
-Bankaccount.all_accounts[2].withdraw(3000/2)
-
-Bankaccount.total_funds
+puts "Book available: #{Book.borrowed}"
+puts Book.available
+Book.borrowed[0].borrow
+Book.borrowed[0].borrow
+Book.borrowed[0].borrow
+puts "Book available: #{Book.borrowed}"
+puts "Book available: #{Book.available}"
