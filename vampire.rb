d6ed6818130a6c2d3e3e7e2d941e8af6a9395f2e
codonculous <feidameng01@gmail.com>
2017-03-31 00:05:46 -0400

    create class methods: create, sunrise and sunset

--- vampire.rb
+++ vampire.rb
@@ -1,5 +1,21 @@
 class Vampire
 
+  @@blood_suckers = []
+
+  def self.create(name,age)
+    @@blood_suckers.push( Vampire.new(name,age) )
+  end
+
+  def self.sunrise
+    killed_v = @@blood_suckers.select {|v| (v.in_coffin == false || v.drank_blood_today == false) }
+    @@blood_suckers.delete(killed_v)
+  end
+
+  def self.sunset
+    @@blood_suckers.map { |v| v.in_coffin = false, v.drank_blood_today = false    }
+
+  end
+
   def initialize(name,age)
     @name = name
     @age = age
